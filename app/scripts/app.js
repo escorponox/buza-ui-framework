949abd6774f61ff37b700f9e1f37a38d92c4affd
Chris Watland <chris.watland@gmail.com>
2017-02-25 20:13:59 -0600

    add accordions

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,6 +13,7 @@ WebFont.load({
     },
 });
 
+/* tabs placeholder */
 const injectPlaceholderIntoTabs = () => {
   const panelArray = Array.from(document.querySelectorAll(".c-tab__panel"));
   panelArray.forEach((panel, index) => {
@@ -21,6 +22,67 @@ const injectPlaceholderIntoTabs = () => {
     panel.appendChild(tabsTemplate);
   });
 };
+/* accordion placeholder content */
+ for (var i = 0; i < 5; i++) {
+
+            var topics = [
+  {
+    'id': 'topic--1',
+    'title': 'SECTION 1'
+  },
+  {
+    'id': 'topic--2',
+    'title': 'SECTION 2'
+  },
+  {
+    'id': 'topic--3',
+    'title': 'SECTION 3'
+  },
+  {
+    'id': 'topic--4',
+    'title': 'SECTION 4'
+  },
+  {
+    'id': 'topic--5',
+    'title': 'SECTION 5'
+  }
+];
+
+var links = [
+  {
+    'id': 'link--1',
+    'title': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at mauris ut ligula euismod rhoncus. Nulla facilisi. Duis a lacus cursus, euismod velit ornare, ultricies lectus. Integer semper facilisis velit et dignissim. Suspendisse potenti. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla tristique mauris lacus. Ut diam velit, aliquam vel nibh eu, consequat sodales diam. Nunc rutrum nisi nunc, eu efficitur eros blandit vel. Vestibulum cursus elit vel risus egestas, dictum sollicitudin risus fermentum.'
+  },
+  {
+    'id': 'link--2',
+    'title': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at mauris ut ligula euismod rhoncus. Nulla facilisi. Duis a lacus cursus, euismod velit ornare, ultricies lectus. Integer semper facilisis velit et dignissim. Suspendisse potenti. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla tristique mauris lacus. Ut diam velit, aliquam vel nibh eu, consequat sodales diam. Nunc rutrum nisi nunc, eu efficitur eros blandit vel. Vestibulum cursus elit vel risus egestas, dictum sollicitudin risus fermentum.'
+  },
+  {
+    'id': 'link--3',
+    'title': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at mauris ut ligula euismod rhoncus. Nulla facilisi. Duis a lacus cursus, euismod velit ornare, ultricies lectus. Integer semper facilisis velit et dignissim. Suspendisse potenti. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla tristique mauris lacus. Ut diam velit, aliquam vel nibh eu, consequat sodales diam. Nunc rutrum nisi nunc, eu efficitur eros blandit vel. Vestibulum cursus elit vel risus egestas, dictum sollicitudin risus fermentum.'
+  },
+  {
+    'id': 'link--4',
+    'title': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at mauris ut ligula euismod rhoncus. Nulla facilisi. Duis a lacus cursus, euismod velit ornare, ultricies lectus. Integer semper facilisis velit et dignissim. Suspendisse potenti. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla tristique mauris lacus. Ut diam velit, aliquam vel nibh eu, consequat sodales diam. Nunc rutrum nisi nunc, eu efficitur eros blandit vel. Vestibulum cursus elit vel risus egestas, dictum sollicitudin risus fermentum.'
+  },
+  {
+    'id': 'link--5',
+    'title': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at mauris ut ligula euismod rhoncus. Nulla facilisi. Duis a lacus cursus, euismod velit ornare, ultricies lectus. Integer semper facilisis velit et dignissim. Suspendisse potenti. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla tristique mauris lacus. Ut diam velit, aliquam vel nibh eu, consequat sodales diam. Nunc rutrum nisi nunc, eu efficitur eros blandit vel. Vestibulum cursus elit vel risus egestas, dictum sollicitudin risus fermentum.'
+  }
+];
+// populate topic
+var topicTemplate = document.querySelector(".js-topic-template").content.cloneNode(true);
+topicTemplate.querySelector(".js-topic__input").setAttribute("id", topics[i].id);  
+topicTemplate.querySelector(".js-topic__overview").setAttribute("for", topics[i].id);
+topicTemplate.querySelector(".js-topic__title").innerHTML = topics[i].title;
+// populate link
+var linkTemplate = document.querySelector(".js-link-template").content.cloneNode(true);
+linkTemplate.querySelector(".js-link__title").innerHTML = links[i].title;
+// append templates
+topicTemplate.querySelector(".js-topic__content").appendChild(linkTemplate);
+document.querySelector(".js-card").appendChild(topicTemplate);
+}
+
 
 documentReady(carousels);
 documentReady(scrollManagement);
