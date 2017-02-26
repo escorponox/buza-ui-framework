9ca3f035c634918aa4099dec15177b480187cb8e
Carlos Coves Prieto <escorponox@gmail.com>
2017-02-26 08:22:54 +0100

    Update 2017-02-26T07:22:52.338Z

--- app/scripts/menu-highlight.js
+++
@@ -1,3 +0,0 @@
-export default (articles) => Array.from(articles).reverse().find(
-  (article) => article.getBoundingClientRect().top + window.pageYOffset < window.pageYOffset + (window.innerHeight / 2)
-).getAttribute('id')
